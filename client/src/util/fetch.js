d8ffbac1aeaf9fe1b0b0bb19e092a64a05a2ebd8
Ján Vorčák <jvorcak@users.noreply.github.com>
2016-06-10 01:43:13 +0200

    Update fetch.js

--- client/src/util/fetch.js
+++ client/src/util/fetch.js
@@ -3,7 +3,7 @@ import 'rxjs/add/observable/fromPromise'
 import 'rxjs/add/operator/mergeMap'
 
 global.self = global
-require('imports?this=>global!exports?global.fetch!isomorphic-fetch')
+//require('imports?this=>global!exports?global.fetch!isomorphic-fetch')
 
 export default function fetchJSON(url) {
   return Observable.fromPromise(fetch(url))
